4e6971e6d79dc5ab35b3cb553d90a76d3e1f105e
Robert Ayrapetyan <robert@yag.im>
2024-11-12 13:38:48 -0800

    drop: devcontainers support

--- .devcontainer/post-create.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /workspaces/jukebox/.vscode
-cp /workspaces/jukebox/.devcontainer/artifacts/vscode/* /workspaces/jukebox/.vscode
-
-sudo cp -r /workspaces/jukebox/runners/base/deps/gstreamer/subprojects/packages/* /usr
