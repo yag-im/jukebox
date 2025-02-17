97d30f85f205849ed7a10d29dc83a63269370940
Robert Ayrapetyan <robert@yag.im>
2025-02-16 19:21:12 -0800

    fix: missing fluidsynth fonts in dosbox-x image fix: images pull script

--- scripts/pull_images.sh
+++ scripts/pull_images.sh
@@ -6,11 +6,11 @@ set -o allexport
 set +o allexport
 
 pull_docker_images() {
-    docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_dosbox:${DOSBOX_VER} \
+    echo "docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_dosbox:${DOSBOX_VER} \
         && docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_dosbox-x:${DOSBOX_X_VER} \
         && docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_dosbox-staging:${DOSBOX_STAGING_VER} \
         && docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_scummvm:${SCUMMVM_VER} \
-        && docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_wine:${WINE_VER}
+        && docker pull $DOCKER_REPO/${WINDOW_SYSTEM}_${VIDEO_ENC}_wine:${WINE_VER}"
 }
 
 pull_local_all() {
@@ -23,7 +23,7 @@ pull_local_mk_all() {
     PORTS=("2201" "2202")
     for port in "${PORTS[@]}"; do
         ssh -i ~/.vagrant.d/insecure_private_keys/vagrant.key.rsa vagrant@127.0.0.1 -p $port \
-            "$(declare -f pull_docker_images); pull_docker_images"
+            "$(pull_docker_images)"
     done
 }
 
@@ -31,7 +31,7 @@ pull_cloud_dev_all() {
     HOST_IPS=("192.168.12.2" "192.168.13.2")
     for host_ip in "${HOST_IPS[@]}"; do
         ssh -i ${BASTION_SECRETS_DIR}/dev/id_ed25519 -o ProxyCommand="ssh -p 2207 -W %h:%p infra@bastion.dev.yag.im" debian@$host_ip \
-            "$(declare -f pull_docker_images); pull_docker_images"
+            "$(pull_docker_images)"
     done
 }
 
@@ -39,7 +39,7 @@ pull_cloud_prod_all() {
     HOST_IPS=("192.168.12.2" "192.168.13.2")
     for host_ip in "${HOST_IPS[@]}"; do
         ssh -i ${BASTION_SECRETS_DIR}/prod/id_ed25519 -o ProxyCommand="ssh -p 2207 -W %h:%p infra@bastion.yag.im" debian@$host_ip \
-            "$(declare -f pull_docker_images); pull_docker_images"
+            "$(pull_docker_images)"
     done
 }
 
